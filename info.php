8aa0a67b98fd19eea8f3b84de0a54d7422885d9b
anapetro <48393206+anapetro@users.noreply.github.com>
2020-04-28 19:44:35 -0500

    update

--- info.php
+++ info.php
@@ -4,24 +4,27 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method == 'GET'){
 $usuarios = [[
-    "Cedula" => 1234,
-    "Nombre" => "Saul rivera",
-    "Direccion" => "calle sin salida",
+    "Cedula" => 123432362,
+    "Nombre" => "Ana ",
+    "Apellido" => " Petro",
+    "Direccion" => "calle 26n 36",
     "Telefono" => "036666",
     "Edad" => 36
 ],
 [
-    "Cedula" => 12345,
-    "Nombre" => "Ashley rivera",
-    "Direccion" => "Calle llena de vida",
+    "Cedula" => 123124545,
+    "Nombre" => "jesus ",
+    "Apellido" => " Petro",
+    "Direccion" => "Calle 62n 35-695",
     "Telefono" => "035555",
     "Edad" => 6
 ]
 ,
 [
-    "Cedula" => 123456,
-    "Nombre" => "Jimena villadiego",
-    "Direccion" => "Calle sin vida",
+    "Cedula" => 13265512456,
+    "Nombre" => "paola ",
+    "Apellido" => " Petro",
+    "Direccion" => "Calle 3512 n 66124mn",
     "Telefono" => "034444",
     "Edad" => 25
 ]];
